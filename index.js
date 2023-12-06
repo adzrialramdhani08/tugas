1dfbc32b69954ce43823d846db6f9482b6bd4732
adzrialramdhani08 <114473430+adzrialramdhani08@users.noreply.github.com>
2023-12-06 22:57:00 +0700

    index.js

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-const mahasiswaNim = 1102010; // Replace with the actual nim
-const updatedData = {
-  nama: 'Messi',
-  gender: 'L',
-  prodi: 'TI',
-  alamat: 'Jl. Cibolang Kaler'
+const { response } = require("express");
+
+const mahasiswaNIM = "1102020";
+const updateData = {
+  nama: "Ronaldo",
+  gender: "L",
+  prodi: "TE",
+  alamat: "JL. Cibolang Kaler",
 };
 
-async function editEntry(id, payload) {
-    const response = fetch(`http://localhost:3000/mahasiswa/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: payload
-    })
-    // .then(response => response.json())
-    // .then(data => console.log(data))
-    // .catch(error => console.error('Error:', error));
-    console.log(response)
-}
-
-editEntry(mahasiswaNim, JSON.stringify(updatedData))
-
-async function createData(payload) {
-    try {
-        const response = await fetch()
-    } catch (error) {
-        console.error(error.message)
-    }
-}
+fetch(`http://localhost:3000/mahasiswa/${mahasiswaNIM}`, {
+  method: "PUT",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(updateData),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.error("error : ", err));
