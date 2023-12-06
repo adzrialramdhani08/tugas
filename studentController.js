ef49da83d635ed44680e759134d800e7e734e33f
adzrialramdhani08 <114473430+adzrialramdhani08@users.noreply.github.com>
2023-12-06 22:54:33 +0700

    mahasiswaControllers.js

--- studentController.js
+++ studentController.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
+const { error } = require("console");
+
+const tableName = "mahasiswa";
 
 // GET all mahasiswa
 router.get('/', (req, res) => {
@@ -31,50 +34,65 @@ router.get('/:nim', (req, res) => {
 
 // POST create new mahasiswa
 router.post('/', (req, res) => {
+  const mahasiswaNIM = req.params.nim;
   const { nim, nama, gender, prodi, alamat } = req.body;
-  db.query(
-    'INSERT INTO mahasiswa (nim, nama, gender, prodi, alamat) VALUES (?, ?, ?, ?, ?)',
-    [nim, nama, gender, prodi, alamat],
-    (error) => {
+  db.query(`UPDATE ${tableName} SET nama = ?, gender = ?, prodi = ?, alamat = ? WHERE nim = ?`, [nama, gender, prodi, alamat, mahasiswaNIM], (error) => {
+   
       if (error) {
         console.error('Error creating mahasiswa:', error);
         res.status(500).json({ message: 'Internal Server Error' });
       } else {
         res.json({ message: 'Mahasiswa created successfully' });
       }
-    }
-  );
-});
+    });
+  });
 
 // PUT update mahasiswa by nim
 router.put('/:nim', (req, res) => {
   const mahasiswaNim = req.params.nim;
   const { nama, gender, prodi, alamat } = req.body;
-  db.query(
-    'UPDATE mahasiswa SET nama = ?, gender = ?, prodi = ?, alamat = ? WHERE nim = ?',
-    [nama, gender, prodi, alamat, mahasiswaNim],
-    (error) => {
+  db.query(`UPDATE ${tableName} SET nama = ?, gender = ?, prodi = ?, alamat = ? WHERE nim = ?`, [nama, gender, prodi, alamat, mahasiswaNIM], (error) => {
       if (error) {
         console.error('Error updating mahasiswa:', error);
         res.status(500).json({ message: 'Internal Server Error' });
-      } else {
+      } else if (result.length === 0) {
         res.json({ message: 'Updating mahasiswa successfully' });
+      } else {
+        res.json(result[0]);
       }
-    }
-  );
-});
+    });
+  });
 
 // DELETE specific mahasiswa by nim
-router.delete('/:nim', (req, res) => {
+router.put("/:nim", (req, res) => {
   const mahasiswaNim = req.params.nim;
+  const { nama, gender, prodi, alamat } = req.body;
   db.query('DELETE FROM mahasiswa WHERE nim = ?', [mahasiswaNim], (error, results) => {
     if (error) {
-      console.error('Error deleting mahasiswa:', error);
+      console.error("Error updating mahasiswa : ", error);
       res.status(500).json({ message: 'Internal Server Error' });
-    } else if (results.affectedRows === 0) {
-      res.status(404).json({ message: 'Mahasiswa not found' });
     } else {
       res.json({ message: 'Mahasiswa deleted successfully' });
+    }
+  });
+});
+
+router.post("/:nim", (req, res) => {
+  const mahasiswaNIM = req.params.nim;
+  const { nama, gender, prodi, alamat } = req.body;
+  const data = {
+    nama: nama,
+    nim: mahasiswaNIM,
+    gender: gender,
+    prodi: prodi,
+    alamat: alamat,
+  };
+   db.query(`INSERT INTO ${tableName} SET nama = ?, gender = ?, prodi = ?, alamat = ? WHERE nim = ?`, [nama, gender, prodi, alamat, mahasiswaNIM], (error) => {
+    if (error) {
+      console.error("Error updating mahasiswa : ", error);
+      res.status(500).json({ massage: "Internal Server Error" });
+    } else {
+      res.json({ massage: "Updatin mahasiswa successfullys" });
     }
   });
 });
